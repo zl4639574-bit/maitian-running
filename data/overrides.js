a185b07972e4d225d86c32da5ec23c801215cd45
zl4639574-bit <zl4639574@gmail.com>
2026-09-14 19:17:08 +0800

    自检：写入测试标记

--- data/overrides.js
+++ data/overrides.js
@@ -505,5 +505,6 @@ window.TEAM_OVERRIDES = {
  "hall": null,
  "queue": null,
  "photos": [],
- "updated": "2026-09-14T10:59:16.406Z"
+ "updated": "2026-09-14T10:59:16.406Z",
+ "selfTest": 1789384626636
 };
