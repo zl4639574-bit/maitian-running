f432a55b328884aa926d01aea9f48520cae4c6fa
zl4639574-bit <zl4639574@gmail.com>
2026-09-14 21:00:47 +0800

    更新队伍数据（队伍信息/荣誉/名册/成绩/照片）

--- data/overrides.js
+++ data/overrides.js
@@ -488,7 +488,63 @@ window.TEAM_OVERRIDES = {
    ]
   }
  },
- "newMembers": [],
+ "newMembers": [
+  {
+   "name": "批量甲",
+   "college": "林学院",
+   "major": "林学2101",
+   "grade": "2023",
+   "sex": "男",
+   "level": [
+    "正式"
+   ],
+   "addedAt": "2026-09-14"
+  },
+  {
+   "name": "批量乙",
+   "college": "园艺学院",
+   "major": "园艺2102",
+   "grade": "2023",
+   "sex": "女",
+   "level": [
+    "预备"
+   ],
+   "addedAt": "2026-09-14"
+  },
+  {
+   "name": "批量丙",
+   "college": "水建学院",
+   "major": "水利2103",
+   "grade": "2024",
+   "sex": "男",
+   "level": [
+    "正式"
+   ],
+   "addedAt": "2026-09-14"
+  },
+  {
+   "name": "文件甲",
+   "college": "动科学院",
+   "major": "动科2101",
+   "grade": "2023",
+   "sex": "男",
+   "level": [
+    "正式"
+   ],
+   "addedAt": "2026-09-14"
+  },
+  {
+   "name": "文件乙",
+   "college": "食品学院",
+   "major": "食工2102",
+   "grade": "2024",
+   "sex": "女",
+   "level": [
+    "预备"
+   ],
+   "addedAt": "2026-09-14"
+  }
+ ],
  "results": [
   {
    "uid": "rmu114vax_1_vq0n",
@@ -607,5 +663,5 @@ window.TEAM_OVERRIDES = {
    "caption": "NR8AVjViQ1FBMk5qRT"
   }
  ],
- "updated": "2026-09-14T12:55:16.472Z"
+ "updated": "2026-09-14T13:00:45.545Z"
 };
