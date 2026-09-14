a6440f048713234893385f4ad4716b45c5be8aae
zl4639574-bit <zl4639574@gmail.com>
2026-09-14 17:29:49 +0800

    更新队伍数据（队伍信息/荣誉/名册/成绩/照片）

---
+++ data/overrides.js
@@ -0,0 +1,17 @@
+window.TEAM_OVERRIDES = {
+ "team": {
+  "slogan": "密钥拦截修复自检"
+ },
+ "honors": null,
+ "activities": null,
+ "hidden": [],
+ "memberEdits": {},
+ "results": [],
+ "competitions": [],
+ "compRecords": {},
+ "hiddenRecords": [],
+ "hall": null,
+ "queue": null,
+ "photos": [],
+ "updated": "2026-09-14T09:29:47.431Z"
+};
