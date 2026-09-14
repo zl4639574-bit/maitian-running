cf0fe51cf603fff5edd7338bc38cba34d75b8d6a
zl4639574-bit <zl4639574@gmail.com>
2026-09-14 18:44:52 +0800

    自检：写入测试标记

---
+++ data/overrides.js
@@ -0,0 +1,15 @@
+/* 由队长版写入 */
+window.TEAM_OVERRIDES = {
+ "team": {},
+ "honors": null,
+ "activities": null,
+ "hidden": [],
+ "memberEdits": {},
+ "results": [],
+ "photos": [],
+ "competitions": [],
+ "hiddenRecords": [],
+ "hall": null,
+ "queue": null,
+ "selfTest": 1789382691208
+};
